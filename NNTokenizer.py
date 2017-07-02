2b655f7b51ae4de2227214d7a5f6f087d4872e81
Joapfel <kraemer_johannes@gmx.de>
2017-07-02 20:26:59 +0200

    added NNTokenizer

---
+++ NNTokenizer.py
@@ -0,0 +1,57 @@
+import numpy as np
+import scipy
+from sklearn import linear_model
+from sklearn import metrics
+from scipy.sparse import csr_matrix
+from scipy.sparse import hstack, vstack
+
+from Tokenizer import Tokenizer
+from keras.models import Sequential
+from keras.layers import Dense, Activation
+from keras import optimizers, losses
+from keras.models import Sequential
+
+t = Tokenizer()
+char_label = t.read_in('e04-data/train')
+
+#fill char2id
+for c in char_label:
+    if c[0] not in t.char2id: #if the char is not in the char2id add it
+        t.char2id.append(c[0])
+t.char2id.append(t.unseen_char)
+
+#fill label2id
+for l in char_label:
+    if l[1] not in t.label2id: #if the label is not in the label2id add it
+        t.label2id.append(l[1])
+
+#get onehots for all training characters
+chars = []
+for tpl in char_label:
+    chars.append(tpl[0])
+X_train = t.one_hot(chars) #onehots of chars
+
+#get classes for training characters
+labels = []
+for tpl in char_label:
+    labels.append(tpl[1])
+y_train = t.labels_2_numbers(labels)
+
+
+"""
+NN with a single layer; uses +-2 context window of onehots
+"""
+X_train  = t.context_window(X_train)
+
+single_layer_model = Sequential()
+single_layer_model.add(Dense(1, input_dim=490))
+print('stage 1')
+single_layer_model.compile(optimizer='adam', loss='mean_squared_error')
+print('stage 2')
+single_layer_model.fit(X_train, y_train, epochs=5)
+
+print('JUHU')
+
+
+
+
--- Tokenizer.py
+++ Tokenizer.py
@@ -23,7 +23,7 @@ class Tokenizer(object):
                 if len(line) == 2:
                     chars_labels.append((line[0], line[1])) #add tuples of the form (char, label) to the list
                 elif len(line) == 1:
-                    chars_labels.append(('<wb>', line[0])) #<wb> == word boundary
+                    chars_labels.append((' ', line[0])) #<w> == whitespace
         return chars_labels
 
     def one_hot(self, characters): #arg = list of characters
@@ -43,136 +43,175 @@ class Tokenizer(object):
             idx.append(self.label2id.index(l))
         return np.array(idx)
 
+    def context_window(self, m):
+        # add padding to the training data
+        padding = np.zeros((2, m.shape[1]))
+        m_padding = np.vstack((padding, m))  # add two zero rows add the beginning
+        m_padding = np.vstack((m_padding, padding))  # add two zero rows add the end
 
-"""
-Simple logistic classifier (maxent) that predicts the class of the character only from its
-one-hot representation
-"""
-##########################################################################
-#TRAIN
-##########################################################################
-t = Tokenizer()
-char_label = t.read_in('e04-data/train') #list of touples (char, label)
+        m_window = csr_matrix(m_padding[:5].flatten())
+        for i in range(1, len(m_padding) - 4):
+            m_window = vstack((m_window, csr_matrix(m_padding[i:i + 5].flatten())))
+        return m_window
 
-#fill char2id
-for c in char_label:
-    if c[0] not in t.char2id: #if the char is not in the char2id add it
-        t.char2id.append(c[0])
-t.char2id.append(t.unseen_char)
-
-#fill label2id
-for l in char_label:
-    if l[1] not in t.label2id: #if the label is not in the label2id add it
-        t.label2id.append(l[1])
-
-#get onehots for all training characters
-chars = []
-for tpl in char_label:
-    chars.append(tpl[0])
-X_train = t.one_hot(chars) #onehots of chars
-
-#get classes for training characters
-labels = []
-for tpl in char_label:
-    labels.append(tpl[1])
-y_train = t.labels_2_numbers(labels)
-
-#train simple logistic classifier
-logreg = linear_model.LogisticRegression()
-logreg.fit(csr_matrix(X_train), y_train)
-
-##########################################################################
-#PREDICT
-##########################################################################
-
-#read in test data
-test_data = t.read_in('e04-data/test') #list of touples (char, label)
-chars_test = []
-for tpl in test_data: #get the unseen characters for prediction
-    chars_test.append(tpl[0])
-labels_test = []
-for tpl in test_data:
-    labels_test.append(tpl[1])
-
-#prepare test data
-X_predict = t.one_hot(chars_test) #onehot representation of the unseen data
-y_correct = t.labels_2_numbers(labels_test) #the correct target classes
-y_predicted = logreg.predict(csr_matrix(X_predict)) #the predicted classes
-
-print('##############################################')
-print('RESULTS WITHOUT CONTEXT WINDOW: ')
-
-accuracy = metrics.accuracy_score(y_correct, y_predicted)
-print('accuracy: ', accuracy)
-
-precision = metrics.precision_score(y_correct, y_predicted, average='macro')
-print('precision: ', precision)
-
-recall = metrics.recall_score(y_correct, y_predicted, average='macro')
-print('recall: ', recall)
-
-f1_score = metrics.f1_score(y_correct, y_predicted, average='macro')
-print('f1_score: ', f1_score)
-print('##############################################')
-print()
+    def tokenize(self, s, lc): #lc is the classifier
+        if not isinstance(s, str):
+            raise TypeError('input s needs to be a string')
+        characters = list(s)
+        characters_onehot = self.one_hot(characters)
+        characters_context = self.context_window(characters_onehot)
+        labels_num = lc.predict(characters_context)
+        labels_ch = []
+        for lbl in labels_num:
+            labels_ch.append(t.label2id[lbl])
+        sentences = []
+        sent = []
+        token = ""
+        for c, l in zip(characters, labels_ch):
+            if l == 'I':
+                token += c
+            elif l == 'T' or l == 'S':
+                if len(token) > 0:
+                    sent.append(token) #add token to the sentence
+                token = c #reset token and add first char of the next token
+                if l == 'S' and len(sent) > 0:
+                    sentences.append(sent)
+                    sent = []
+        #at the end add the last token and sent to its higher structure
+        if len(token) > 0:
+            sent.append(token)
+        if len(sent) > 0:
+            sentences.append(sent)
+        return sentences
 
 
-"""
-logistic classifier (maxent) that predicts the class of the character using onehot representation
-with +-2 window
-"""
 
-#add padding to the training data
-padding = np.zeros((2, X_train.shape[1]))
-X_train_padding = np.vstack((padding, X_train)) #add two zero rows add the beginning
-X_train_padding = np.vstack((X_train_padding, padding)) #add two zero rows add the end
+if '__main__' == __name__:
 
-#create sparse matrix
-X_train_window = csr_matrix(X_train_padding[:5].flatten())
-for i in range(1, len(X_train_padding) - 4):
-    X_train_window = vstack((X_train_window, csr_matrix(X_train_padding[i:i+5].flatten())))
+    """
+    Simple logistic classifier (maxent) that predicts the class of the character only from its
+    one-hot representation
+    """
+    ##########################################################################
+    #TRAIN
+    ##########################################################################
+    t = Tokenizer()
+    char_label = t.read_in('e04-data/train') #list of touples (char, label)
 
-#train logistic classifier with window +-2
-logreg_2 = linear_model.LogisticRegression()
-logreg_2.fit(X_train_window, y_train)
+    #fill char2id
+    for c in char_label:
+        if c[0] not in t.char2id: #if the char is not in the char2id add it
+            t.char2id.append(c[0])
+    t.char2id.append(t.unseen_char)
 
-#reuse test data from simple classifier
-padding = np.zeros((2, X_predict.shape[1]))
-X_2 = np.vstack((padding, X_predict)) #padded test data
-X_2 = np.vstack((X_2, padding))
+    #fill label2id
+    for l in char_label:
+        if l[1] not in t.label2id: #if the label is not in the label2id add it
+            t.label2id.append(l[1])
 
-X_predict_2 = csr_matrix(X_2[:5].flatten())
-for i in range(1, len(X_2)-4):
-    X_predict_2 = vstack((X_predict_2, csr_matrix(X_2[i:i+5].flatten())))
-#y_correct -> use same labels
-y_predicted_2 = logreg_2.predict(X_predict_2)
+    #get onehots for all training characters
+    chars = []
+    for tpl in char_label:
+        chars.append(tpl[0])
+    X_train = t.one_hot(chars) #onehots of chars
 
-print('##############################################')
-print('RESULTS WITH +-2 CONTEXT WINDOW: ')
+    #get classes for training characters
+    labels = []
+    for tpl in char_label:
+        labels.append(tpl[1])
+    y_train = t.labels_2_numbers(labels)
 
-accuracy = metrics.accuracy_score(y_correct, y_predicted_2)
-print('accuracy: ', accuracy)
+    #train simple logistic classifier
+    logreg = linear_model.LogisticRegression()
+    logreg.fit(csr_matrix(X_train), y_train)
 
-precision = metrics.precision_score(y_correct, y_predicted_2, average='macro')
-print('precision: ', precision)
+    ##########################################################################
+    #PREDICT
+    ##########################################################################
 
-recall = metrics.recall_score(y_correct, y_predicted_2, average='macro')
-print('recall: ', recall)
+    #read in test data
+    test_data = t.read_in('e04-data/test') #list of touples (char, label)
+    chars_test = []
+    for tpl in test_data: #get the unseen characters for prediction
+        chars_test.append(tpl[0])
+    labels_test = []
+    for tpl in test_data:
+        labels_test.append(tpl[1])
 
-f1_score = metrics.f1_score(y_correct, y_predicted_2, average='macro')
-print('f1_score: ', f1_score)
-print('##############################################')
-print()
+    #prepare test data
+    X_predict = t.one_hot(chars_test) #onehot representation of the unseen data
+    y_correct = t.labels_2_numbers(labels_test) #the correct target classes
+    y_predicted = logreg.predict(csr_matrix(X_predict)) #the predicted classes
+
+    print('##############################################')
+    print('RESULTS WITHOUT CONTEXT WINDOW: ')
+
+    accuracy = metrics.accuracy_score(y_correct, y_predicted)
+    print('accuracy: ', accuracy)
+
+    precision = metrics.precision_score(y_correct, y_predicted, average='macro')
+    print('precision: ', precision)
+
+    recall = metrics.recall_score(y_correct, y_predicted, average='macro')
+    print('recall: ', recall)
+
+    f1_score = metrics.f1_score(y_correct, y_predicted, average='macro')
+    print('f1_score: ', f1_score)
+    print('##############################################')
+    print()
 
 
-"""
-confusion matrix for the simple classifier results vs the classifier with context window
-"""
-print('confusion matrix for simple classifier')
-print(metrics.confusion_matrix(y_correct, y_predicted))
-print()
-print('confusion matrix for classifier with +-2 context window')
-print(metrics.confusion_matrix(y_correct, y_predicted_2))
+    """
+    logistic classifier (maxent) that predicts the class of the character using onehot representation
+    with +-2 window
+    """
+    ##########################################################################
+    #TRAIN
+    ##########################################################################
+
+    logreg_2 = linear_model.LogisticRegression()
+    logreg_2.fit(t.context_window(X_train), y_train)
 
 
+    ##########################################################################
+    #PREDICT
+    ##########################################################################
+
+    y_predicted_2 = logreg_2.predict(t.context_window(X_predict))
+
+    print('##############################################')
+    print('RESULTS WITH +-2 CONTEXT WINDOW: ')
+
+    accuracy = metrics.accuracy_score(y_correct, y_predicted_2)
+    print('accuracy: ', accuracy)
+
+    precision = metrics.precision_score(y_correct, y_predicted_2, average='macro')
+    print('precision: ', precision)
+
+    recall = metrics.recall_score(y_correct, y_predicted_2, average='macro')
+    print('recall: ', recall)
+
+    f1_score = metrics.f1_score(y_correct, y_predicted_2, average='macro')
+    print('f1_score: ', f1_score)
+    print('##############################################')
+    print()
+
+
+    """
+    confusion matrix for the simple classifier results vs the classifier with context window
+    """
+    print('confusion matrix for simple classifier')
+    print(metrics.confusion_matrix(y_correct, y_predicted))
+    print()
+    print('confusion matrix for classifier with +-2 context window')
+    print(metrics.confusion_matrix(y_correct, y_predicted_2))
+
+
+    """
+    use the trained model to tokenize some input
+    """
+    print()
+    test1 = 'From the AP comes this story : President Bush on Tuesday nominated two individuals to replace retiring jurists on federal courts in the Washington area.'
+    tok = t.tokenize(test1, logreg_2)
+    print(tok)
 
